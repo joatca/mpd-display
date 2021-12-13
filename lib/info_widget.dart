d5552f310634e94af969ea206318cab250d2b52d
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-12 22:45:08 -0500

    add some more fonts and themes

---
+++ lib/info_widget.dart
@@ -0,0 +1,180 @@
+/*
+This file is part of MPD Display - an MPD status display for viewing across a room
+
+Copyright (C) 2021 Fraser McCrossan
+
+MPD Display is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'data_classes.dart';
+import 'mpd_client.dart';
+
+class InfoWidget extends StatefulWidget {
+  const InfoWidget({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<InfoWidget> createState() => _InfoWidgetState();
+}
+
+class _InfoWidgetState extends State<InfoWidget> {
+  var _state = Info();
+  var mpd = MPDClient();
+  late Stream<Info> infoStream;
+  StreamSubscription<Info>? subscription = null;
+
+  _InfoWidgetState() {
+    infoStream = mpd.infoStream();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startListening();
+  }
+
+  @override
+  void dispose() {
+    stopListening();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var bar = AppBar(
+      title: Text(widget.title),
+      actions: <Widget>[
+        const IconButton(
+            onPressed: null,
+            icon: const Icon(Icons.skip_previous),
+            tooltip: 'Previous'),
+        const IconButton(
+            onPressed: null, icon: const Icon(Icons.pause), tooltip: 'Album'),
+        const IconButton(
+            onPressed: null,
+            icon: const Icon(Icons.skip_next),
+            tooltip: 'Pause'),
+        const IconButton(
+            onPressed: null,
+            icon: const Icon(Icons.sync_problem),
+            tooltip: 'Disconnected'),
+        const IconButton(
+            onPressed: null,
+            icon: const Icon(Icons.block_sharp),
+            tooltip: 'Disconnected'),
+      ].map((w) => Transform.scale(scale: 1.5, child: w)).toList(),
+    );
+    return Scaffold(
+      appBar: bar,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: titleWidget(),
+          ),
+          Expanded(
+            child: subInfoList(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget titleWidget() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Text(
+        _state.info ?? "-",
+        textAlign: TextAlign.center,
+        softWrap: true,
+        maxLines: 4,
+        overflow: TextOverflow.fade,
+        style: Theme.of(context).textTheme.headline1,
+      ),
+    );
+  }
+
+  Widget subInfoList() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children:
+              _state.subInfos.map((i) => subInfoRow(i)).toList(),
+        ),
+      ),
+    );
+  }
+
+  IconData infoTypeToIcon(InfoType type) {
+    switch (type) {
+      case InfoType.album:
+        return Icons.album;
+      case InfoType.composer:
+        return Icons.edit;
+      case InfoType.performer:
+        return Icons.piano;
+      default:
+        return Icons.device_unknown;
+    }
+  }
+
+  Widget subInfoRow(SubInfo i) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(right: 16),
+          child: Icon(infoTypeToIcon(i.type)),
+        ),
+        Flexible(
+          child: Text(
+            i.text,
+            textAlign: TextAlign.left,
+            overflow: TextOverflow.ellipsis,
+            softWrap: true,
+            maxLines: 3,
+            style: Theme.of(context).textTheme.headline2,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void startListening() async {
+    if (subscription == null) {
+      subscription = infoStream.listen((info) {
+        setState(() {
+          _state = info;
+        });
+      });
+    } else {
+      if (subscription?.isPaused ?? false) {
+        subscription?.resume();
+      } else {
+        subscription?.pause();
+      }
+    }
+  }
+
+  void stopListening() {
+    subscription?.cancel();
+    subscription = null;
+  }
+}
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -21,9 +21,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'data_classes.dart';
 import 'mpd_client.dart';
+import 'info_widget.dart';
 
 class PageState {
-  Info info = Info();
   String themeName = "Manuscript";
 
   PageState();
@@ -38,186 +38,116 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
+class InfoTheme {
+  String? font;
+  Color? bgColor;
+  Color? titleColor;
+  Color? infoColor;
+  Color? infoIconColor;
+
+  InfoTheme({
+      this.font,
+      this.bgColor,
+      this.titleColor,
+      this.infoColor,
+      this.infoIconColor
+  });
+}
+
 class _MainPageState extends State<MainPage> {
   static const double _titleSize = 72;
   static const double _infoSize = 56;
 
-  static const themes = {
-    "Plain": {},
-    "Manuscript": {
-      "font": "Baskerville",
-      "bgcolour": Colors.white,
-      "titlecolour": Colors.black,
-      "infocolour": Colors.black,
-    },
-    "Cyberpunk": {
-      "font": "Swerve",
-      "bgcolour": Colors.black,
-      "titlecolor": Colors.pink,
-      "infocolor": Colors.blue,
-    },
+  static var themes = {
+    "Plain": InfoTheme(
+      font: "Roboto",
+      bgColor: Colors.white,
+      titleColor: Colors.black,
+      infoColor: Colors.black,
+    ),
+    "Manuscript": InfoTheme(
+      font: "Baskerville",
+      bgColor: Colors.white,
+      titleColor: Colors.black,
+      infoColor: Colors.black,
+    ),
+    "Negative Manuscript": InfoTheme(
+      font: "Baskerville",
+      bgColor: Colors.black,
+      titleColor: Colors.white,
+      infoColor: Colors.white70,
+      infoIconColor: Colors.white,
+    ),
+    "Classic": InfoTheme(
+      font: "Cardo",
+      bgColor: Colors.white,
+      titleColor: Colors.black,
+      infoColor: Colors.black,
+    ),
+    "Elegance": InfoTheme(
+      font: "Parisienne",
+      bgColor: Colors.white,
+      titleColor: Colors.black,
+      infoColor: Colors.black,
+    ),
+    "Imposing": InfoTheme(
+      font: "Playfair",
+      bgColor: Colors.black,
+      titleColor: Colors.white,
+      infoColor: Colors.white70,
+    ),
+    "8Bit": InfoTheme(
+      font: "PressStart",
+      bgColor: Colors.black,
+      titleColor: Colors.white,
+      infoColor: Colors.red,
+    ),
+    "Cyberpunk": InfoTheme(
+      font: "Swerve",
+      bgColor: Colors.black,
+      titleColor: Colors.pink,
+      infoColor: Colors.blue,
+      infoIconColor: Colors.green,
+    ),
   };
 
   var _state = PageState();
-  var mpd = MPDClient();
-  late Stream<Info> infoStream;
-  StreamSubscription<Info>? subscription = null;
 
   _MainPageState() {
-    infoStream = mpd.infoStream();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    startListening();
-  }
-
-  @override
-  void dispose() {
-    stopListening();
-    super.dispose();
+    // nothing for now
   }
 
   @override
   Widget build(BuildContext context) {
-    var themeMap = themes[_state.themeName];
+    //_state.themeName = "Cyberpunk";
+    //_state.themeName = "Manuscript";
+    _state.themeName = "Negative Manuscript";
+    //_state.themeName = "Classic";
+    //_state.themeName = "Elegance";
+    //_state.themeName = "8Bit";
+    //_state.themeName = "Imposing";
+    var themeInfo = themes[_state.themeName];
     var textTheme = Theme.of(context).textTheme.copyWith(
-          headline1: TextStyle(
-            fontFamily: themeMap?["font"],
-            fontSize: _titleSize,
-            color: themeMap?["titlecolour"],
-          ),
-          headline2: TextStyle(
-            fontFamily: themeMap?["font"],
-            fontSize: _infoSize,
-            color: themeMap?["infocolour"],
-          ),
-        );
+      headline1: TextStyle(
+        fontFamily: themeInfo?.font,
+        fontSize: _titleSize,
+        color: themeInfo?.titleColor,
+      ),
+      headline2: TextStyle(
+        fontFamily: themeInfo?.font,
+        fontSize: _infoSize,
+        color: themeInfo?.infoColor,
+      ),
+    );
+    var iconTheme = Theme.of(context).iconTheme.copyWith(
+      color: themeInfo?.infoIconColor ?? themeInfo?.infoColor,
+      size: _infoSize,
+    );
     var theme = Theme.of(context).copyWith(
       textTheme: textTheme,
-      backgroundColor: themeMap?["bgcolour"],
+      scaffoldBackgroundColor: themeInfo?.bgColor,
+      iconTheme: iconTheme,
     );
-    var bar = AppBar(
-      title: Text(widget.title),
-      actions: <Widget>[
-        const IconButton(
-            onPressed: null,
-            icon: const Icon(Icons.skip_previous),
-            tooltip: 'Previous'),
-        const IconButton(
-            onPressed: null, icon: const Icon(Icons.pause), tooltip: 'Album'),
-        const IconButton(
-            onPressed: null,
-            icon: const Icon(Icons.skip_next),
-            tooltip: 'Pause'),
-        const IconButton(
-            onPressed: null,
-            icon: const Icon(Icons.sync_problem),
-            tooltip: 'Disconnected'),
-        const IconButton(
-            onPressed: null,
-            icon: const Icon(Icons.block_sharp),
-            tooltip: 'Disconnected'),
-      ].map((w) => Transform.scale(scale: 1.5, child: w)).toList(),
-    );
-    return Scaffold(
-      appBar: bar,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: titleWidget(theme),
-          ),
-          Expanded(
-            child: subInfoList(theme),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget titleWidget(ThemeData theme) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Text(
-        _state.info.info ?? "-",
-        textAlign: TextAlign.center,
-        softWrap: true,
-        maxLines: 4,
-        overflow: TextOverflow.fade,
-        style: theme.textTheme.headline1,
-      ),
-    );
-  }
-
-  Widget subInfoList(ThemeData theme) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children:
-              _state.info.subInfos.map((i) => subInfoRow(theme, i)).toList(),
-        ),
-      ),
-    );
-  }
-
-  IconData infoTypeToIcon(InfoType type) {
-    switch (type) {
-      case InfoType.album:
-        return Icons.album;
-      case InfoType.composer:
-        return Icons.edit;
-      case InfoType.performer:
-        return Icons.piano;
-      default:
-        return Icons.device_unknown;
-    }
-  }
-
-  Widget subInfoRow(ThemeData theme, SubInfo i) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: EdgeInsets.only(right: 16),
-          child: Icon(infoTypeToIcon(i.type), size: _infoSize),
-        ),
-        Flexible(
-          child: Text(
-            i.text,
-            textAlign: TextAlign.left,
-            overflow: TextOverflow.ellipsis,
-            softWrap: true,
-            maxLines: 3,
-            style: theme.textTheme.headline2,
-          ),
-        ),
-      ],
-    );
-  }
-
-  void startListening() async {
-    if (subscription == null) {
-      subscription = infoStream.listen((info) {
-        setState(() {
-          _state.info = info;
-        });
-      });
-    } else {
-      if (subscription?.isPaused ?? false) {
-        subscription?.resume();
-      } else {
-        subscription?.pause();
-      }
-    }
-  }
-
-  void stopListening() {
-    subscription?.cancel();
-    subscription = null;
+    return Theme(data: theme, child: const InfoWidget(title: "Foo"));
   }
 }
