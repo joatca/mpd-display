e682328a1b23da187f82eb6fe04a308ba62877d2
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-21 17:58:14 -0500

    factor the info view appbar into a widget

---
+++ lib/info_app_bar.dart
@@ -0,0 +1,212 @@
+/*
+This file is part of MPD Display - an MPD status display for viewing across a room
+
+Copyright (C) 2021 Fraser McCrossan
+
+MPD Display is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'about.dart';
+import 'data_classes.dart';
+import 'info_widget.dart';
+import 'mpd_client.dart';
+
+class InfoAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const InfoAppBar({
+    Key? key,
+    required this.context,
+    required this.infoState,
+    required this.mpd,
+    required this.onSliderChanged,
+  }) : super(key: key);
+
+  final BuildContext context;
+  final InfoState infoState;
+  final MPDClient mpd;
+  final void Function(double v) onSliderChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    final actions = <Widget>[
+      IconButton(
+          onPressed: infoState.info.state == PlayState.stopped
+              ? null
+              : () => mpd.sendCommand("previous"),
+          icon: const Icon(Icons.skip_previous),
+          tooltip: 'Previous'),
+      IconButton(
+          onPressed: () {
+            switch (infoState.info.state) {
+              case PlayState.stopped:
+                mpd.sendCommand("play");
+                break;
+              case PlayState.paused:
+                mpd.sendCommand("pause 0");
+                break;
+              case PlayState.playing:
+                mpd.sendCommand("pause 1");
+            }
+          },
+          icon: infoState.info.state == PlayState.playing
+              ? const Icon(Icons.pause)
+              : const Icon(Icons.play_arrow),
+          tooltip: 'Album'),
+      IconButton(
+          onPressed: infoState.info.state == PlayState.stopped
+              ? null
+              : () => mpd.sendCommand("next"),
+          icon: const Icon(Icons.skip_next),
+          tooltip: 'Pause'),
+      const VerticalDivider(),
+      Consumer<PageState>(
+        builder: (context, pageState, child) => PopupMenuButton<String>(
+          icon: const Icon(Icons.text_format),
+          itemBuilder: (context) {
+            return pageState
+                .themeNames()
+                .map((name) => CheckedPopupMenuItem(
+                      child: Text(name),
+                      value: name,
+                      checked: name == pageState.themeName,
+                    ))
+                .toList();
+          },
+          onSelected: (s) {
+            pageState.setTheme(s);
+          },
+        ),
+      ),
+      IconButton(
+          onPressed: () {
+            _displayTextInputDialog(context, mpd);
+          },
+          icon: const Icon(Icons.settings_ethernet),
+          tooltip: 'Set MPD Server'),
+      IconButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AboutPage()),
+          );
+        },
+        icon: const Icon(Icons.info),
+      ),
+    ].map((w) => Transform.scale(scale: 1.5, child: w)).toList();
+    return AppBar(
+      title: Row(
+        children: [
+          Expanded(
+            child: Slider(
+              onChangeStart: (startVal) {
+                infoState.sliderUpdateEnabled = false;
+                infoState.estimatedElapsed = startVal;
+              },
+              onChanged: onSliderChanged,
+              onChangeEnd: (endVal) {
+                // sanity check in case the track changed during drag
+                infoState.estimatedElapsed = endVal <= infoState.info.duration
+                    ? endVal
+                    : infoState.info.duration - 0.1;
+                infoState.sliderUpdateEnabled = true;
+                mpd.sendCommand("seekcur ${infoState.estimatedElapsed}");
+              },
+              value: infoState.estimatedElapsed,
+              min: 0,
+              // if min == max, slider is disabled, so force that if we are stopped
+              max: infoState.info.state == PlayState.stopped
+                  ? 0
+                  : infoState.info.duration,
+            ),
+          ),
+        ],
+      ),
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  TextEditingValue _ipPort(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.isEmpty) {
+      // we have to allow blanks, otherwise we can't delete the last digit
+      return newValue;
+    }
+    final port = int.tryParse(newValue.text);
+    if (port == null || port <= 0 || port >= 65536) {
+      return oldValue;
+    }
+    return newValue;
+  }
+  
+  Future<void> _displayTextInputDialog(
+      BuildContext context, MPDClient mpd) async {
+    var oneLine = FilteringTextInputFormatter.singleLineFormatter;
+    var portOnly = TextInputFormatter.withFunction(_ipPort);
+    String? mpdServer;
+    int? mpdPort;
+
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("MPD Server"),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    onChanged: (value) {
+                      mpdServer = value;
+                    },
+                    inputFormatters: [oneLine],
+                    controller: TextEditingController(text: mpd.server),
+                    decoration:
+                        const InputDecoration(hintText: "Name or IP address"),
+                  ),
+                  TextField(
+                    onChanged: (value) {
+                      mpdPort = int.parse(
+                          value); // no need to catch exception, ipPort() has validated it already
+                    },
+                    inputFormatters: [portOnly],
+                    controller:
+                        TextEditingController(text: mpd.port.toString()),
+                    decoration: const InputDecoration(
+                        hintText: "Port number (default 6600)"),
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text("Connect"),
+                onPressed: () {
+                  mpd.setServer(mpdServer, mpdPort);
+                  Navigator.pop(context);
+                },
+              ),
+              TextButton(
+                child: const Text("Cancel"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+}
--- lib/info_widget.dart
+++ lib/info_widget.dart
@@ -19,14 +19,12 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 import 'package:wakelock/wakelock.dart';
 import 'data_classes.dart';
 import 'mpd_client.dart';
+import 'info_app_bar.dart';
 import 'title_text.dart';
 import 'subinfo.dart';
-import 'about.dart';
 
 class InfoState {
   Info info = Info();
@@ -53,10 +51,6 @@ class _InfoWidgetState extends State<InfoWidget> with WidgetsBindingObserver {
   int currentScroll = 0;
   int scrollDirection = 1;
   Timer? ticker;
-
-  // used by the dialog to change the MPD server
-  String? mpdServer;
-  int? mpdPort;
 
   _InfoWidgetState();
 
@@ -100,7 +94,20 @@ class _InfoWidgetState extends State<InfoWidget> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: infoAppBar(_state, widget.mpd),
+      appBar: InfoAppBar(
+          context: context,
+          infoState: _state,
+          mpd: widget.mpd,
+          onSliderChanged: (val) {
+            if (val <= _state.info.duration) {
+              setState(() {
+                _state.estimatedElapsed = val;
+              });
+            } else {
+              // if the value is greater than the duration then something weird happened, resync the current status
+              widget.mpd.getStatus();
+            }
+          }),
       body: LayoutBuilder(
         builder: (context, constraints) => _state.info.isEmpty()
             ? emptyLayout(context, constraints)
@@ -109,113 +116,6 @@ class _InfoWidgetState extends State<InfoWidget> with WidgetsBindingObserver {
     );
   }
 
-  AppBar infoAppBar(InfoState infoState, MPDClient mpd) {
-        final actions = <Widget>[
-      IconButton(
-          onPressed: infoState.info.state == PlayState.stopped
-              ? null
-              : () => mpd.sendCommand("previous"),
-          icon: const Icon(Icons.skip_previous),
-          tooltip: 'Previous'),
-      IconButton(
-          onPressed: () {
-            switch (infoState.info.state) {
-              case PlayState.stopped:
-                mpd.sendCommand("play");
-                break;
-              case PlayState.paused:
-                mpd.sendCommand("pause 0");
-                break;
-              case PlayState.playing:
-                mpd.sendCommand("pause 1");
-            }
-          },
-          icon: infoState.info.state == PlayState.playing
-              ? Icon(Icons.pause)
-              : Icon(Icons.play_arrow),
-          tooltip: 'Album'),
-      IconButton(
-          onPressed: infoState.info.state == PlayState.stopped
-              ? null
-              : () => mpd.sendCommand("next"),
-          icon: const Icon(Icons.skip_next),
-          tooltip: 'Pause'),
-      VerticalDivider(),
-      Consumer<PageState>(
-        builder: (context, pageState, child) => PopupMenuButton<String>(
-          icon: const Icon(Icons.text_format),
-          itemBuilder: (context) {
-            return pageState
-                .themeNames()
-                .map((name) => CheckedPopupMenuItem(
-                      child: Text(name),
-                      value: name,
-                      checked: name == pageState.themeName,
-                    ))
-                .toList();
-          },
-          onSelected: (s) {
-            pageState.setTheme(s);
-          },
-        ),
-      ),
-      IconButton(
-          onPressed: () {
-            _displayTextInputDialog(context);
-          },
-          icon: const Icon(Icons.settings_ethernet),
-          tooltip: 'Set MPD Server'),
-      IconButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AboutPage()),
-          );
-        },
-        icon: const Icon(Icons.info),
-      ),
-    ]; //.map((w) => Transform.scale(scale: 1.5, child: w)).toList();
-    return AppBar(
-      title: Row(
-        children: [
-          Expanded(
-            child: Slider(
-              onChangeStart: (startVal) {
-                infoState.sliderUpdateEnabled = false;
-                infoState.estimatedElapsed = startVal;
-              },
-              onChanged: (val) {
-                if (val <= infoState.info.duration) {
-                  setState(() {
-                    infoState.estimatedElapsed = val;
-                  });
-                } else {
-                  // if the value is greater than the duration then something weird happened, resync the current status
-                  mpd.getStatus();
-                }
-              },
-              onChangeEnd: (endVal) {
-                // sanity check in case the track changed during drag
-                infoState.estimatedElapsed = endVal <= infoState.info.duration
-                    ? endVal
-                    : infoState.info.duration - 0.1;
-                infoState.sliderUpdateEnabled = true;
-                mpd.sendCommand("seekcur ${infoState.estimatedElapsed}");
-              },
-              value: infoState.estimatedElapsed,
-              min: 0,
-              // if min == max, slider is disabled, so force that if we are stopped
-              max: infoState.info.state == PlayState.stopped
-                  ? 0
-                  : infoState.info.duration,
-            ),
-          ),
-        ],
-      ),
-      actions: actions,
-    );
-  }
-  
   Widget playingLayout(
       BuildContext context, BoxConstraints constraints, Info info) {
     return Column(
@@ -305,70 +205,5 @@ class _InfoWidgetState extends State<InfoWidget> with WidgetsBindingObserver {
 
   void stopListening() {
     subscription?.pause();
-  }
-
-  TextEditingValue ipPort(
-      TextEditingValue oldValue, TextEditingValue newValue) {
-    if (newValue.text.isEmpty) {
-      // we have to allow blanks, otherwise we can't delete the last digit
-      return newValue;
-    }
-    final port = int.tryParse(newValue.text);
-    if (port == null || port <= 0 || port >= 65536) {
-      return oldValue;
-    }
-    return newValue;
-  }
-
-  Future<void> _displayTextInputDialog(BuildContext context) async {
-    var oneLine = FilteringTextInputFormatter.singleLineFormatter;
-    var portOnly = TextInputFormatter.withFunction(ipPort);
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('MPD Server'),
-            content: SingleChildScrollView(
-              child: Column(
-                children: [
-                  TextField(
-                    onChanged: (value) {
-                      mpdServer = value;
-                    },
-                    inputFormatters: [oneLine],
-                    controller: TextEditingController(text: widget.mpd.server),
-                    decoration: InputDecoration(hintText: "Name or IP address"),
-                  ),
-                  TextField(
-                    onChanged: (value) {
-                      mpdPort = int.parse(
-                          value); // no need to catch exception, ipPort() has validated it already
-                    },
-                    inputFormatters: [portOnly],
-                    controller:
-                        TextEditingController(text: widget.mpd.port.toString()),
-                    decoration:
-                        InputDecoration(hintText: "Port number (default 6600)"),
-                  ),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Connect'),
-                onPressed: () {
-                  widget.mpd.setServer(mpdServer, mpdPort);
-                  Navigator.pop(context);
-                },
-              ),
-              TextButton(
-                child: Text('Cancel'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          );
-        });
   }
 }
