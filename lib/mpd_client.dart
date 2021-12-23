1270aa985464e54051502ba26232d5f644d6c652
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-22 20:59:43 -0500

    refactored a bunch of stuff to eliminate stupid async race conditions; left debugging print statements

--- lib/mpd_client.dart
+++ lib/mpd_client.dart
@@ -47,6 +47,7 @@ class MPDClient {
   Socket? socket;
   ConnState connstate = ConnState.connecting;
   bool stayConnected = false; // whether to reconnect on failure/disconnect
+  bool abnormalDisconnect = false;
   late StreamController<Info> controller;
   final utf8 = const Utf8Codec(allowMalformed: true);
 
@@ -62,14 +63,34 @@ class MPDClient {
     return controller.stream;
   }
 
-  Future<void> connect() async {
-    pid("connect");
-    await loadServerPrefs();
+  void notifyDisconnected() {
+    controller.add(Info(
+      connected: false,
+    ));
+  }
+
+  void connect() {
+    if (kDebugMode) {
+      print("connect");
+    }
     stayConnected = true;
+    connectSocket();
+  }
+
+  Future<void> connectSocket() async {
+    if (kDebugMode) {
+      print("connectSocket");
+    }
+    await loadServerPrefs();
     if (socket == null) {
       // only connect if we aren't already connected
-      pid("connecting");
+      if (kDebugMode) {
+        print("connectSocket: connecting");
+      }
       Socket.connect(server, port, timeout: Duration(seconds: 5)).then((sock) {
+        if (kDebugMode) {
+          print("connectSocket: connected");
+        }
         retryTimer?.cancel();
         retryInterval = minRetryInterval;
         sock.listen(
@@ -80,33 +101,57 @@ class MPDClient {
         socket = sock;
         connstate = ConnState.connecting;
       }).catchError((e) {
-        retry();
+        if (kDebugMode) {
+          print("connectSocket: caught error");
+        }
+        notifyDisconnected();
+        // if we need to retry here there is no valid socket so we can re-call this function
+        retry(connectSocket);
       });
     } else {
       retryTimer?.cancel();
     }
   }
 
-  Future<void> disconnect() async {
-    pid("disconnect");
-    retryTimer?.cancel();
-    pid("flushing");
-    await socket?.flush();
-    pid("closing");
-    await socket?.close();
-    pid("closed");
-    socket = null;
+  void disconnect() {
+    if (kDebugMode) {
+      print("disconnect");
+    }
     stayConnected = false;
+    disconnectSocket();
   }
 
-  Future<void> reconnect() async {
-    await disconnect();
-    await connect();
+  Future<void> disconnectSocket() async {
+    if (kDebugMode) {
+      print("disconnectSocket");
+    }
+    retryTimer?.cancel();
+    if (kDebugMode) {
+      print("disconnectSocket: flushing");
+    }
+    await socket?.flush();
+    if (kDebugMode) {
+      print("disconnectSocket: closing");
+    }
+    await socket?.close();
+    if (kDebugMode) {
+      print("disconnectSocket: closed");
+    }
+    socket = null;
+  }
+
+  Future<void> reconnectSocket() async {
+    if (kDebugMode) {
+      print("reconnectSocket");
+    }
+    abnormalDisconnect = true;
+    await disconnectSocket();
+    abnormalDisconnect = false;
+    await connectSocket();
   }
 
   void onData(Uint8List data) {
     var lines = utf8.decode(data).trim().split("\n");
-    pid("read: $lines");
     switch (connstate) {
       case ConnState.connecting:
         processConnecting(lines);
@@ -119,26 +164,48 @@ class MPDClient {
   }
 
   void onDone() {
-    pid("ondone");
-    retry();
+    if (kDebugMode) {
+      print("onDone");
+    }
+    notifyDisconnected();
+    if (!abnormalDisconnect) {
+      // only do this is we are not in the middle of a reconnect cycle
+      // if onDone is called then the socket is closed so it's safe to discard it
+      socket = null;
+      connectSocket();
+    }
   }
 
   void onError(e) {
-    pid("onerror: $e");
-    socket = null;
-    retry();
+    if (kDebugMode) {
+      print("onError: $e");
+    }
+    notifyDisconnected();
+    // retrying here probably means the socket is dead, but let's make sure
+    retry(() async {
+      if (kDebugMode) {
+        print("retry after error");
+      }
+      await reconnectSocket(); // onDone will reconnect
+    });
   }
 
   Future<void> loadServerPrefs() async {
+    if (kDebugMode) {
+      print("loadServerPrefs");
+    }
     SharedPreferences prefs = await SharedPreferences.getInstance();
     server = prefs.getString('server') ?? "music";
     port = prefs.getInt('port') ?? 6600;
-    if (stayConnected) {
-      await reconnect();
+    if (kDebugMode) {
+      print("loadServerPrefs: found $server:$port");
     }
   }
 
   Future<void> setServer(String? server, int? port) async {
+    if (kDebugMode) {
+      print("setServer");
+    }
     SharedPreferences prefs = await SharedPreferences.getInstance();
     if (server != null) {
       await prefs.setString("server", server);
@@ -148,59 +215,82 @@ class MPDClient {
       await prefs.setInt("port", port);
       this.port = port;
     }
+    if (kDebugMode) {
+      print("setServer: saved");
+    }
     if (stayConnected) {
-      await reconnect();
+      await reconnectSocket();
     }
   }
 
-  Future<void> retry() async {
-    retryTimer?.cancel();
-    if (stayConnected) {
-      controller.add(Info(
-        connected: false,
-      ));
-      await disconnect();
-      retryTimer = Timer(Duration(seconds: retryInterval), connect);
-      pid("retry in $retryTimer");
-      if (retryInterval < maxRetryInterval) {
-        retryInterval += retryIncrement;
-      }
+  Future<void> retry(Future<void> Function() retryAction) async {
+    // if (kDebugMode) {
+    //   print("retry");
+    // }
+    // retryTimer?.cancel();
+    // if (stayConnected) {
+    //   if (kDebugMode) {
+    //     print("retry: sent disconnected notification");
+    //   }
+    //   await disconnectSocket();
+    if (kDebugMode) {
+      print("retry: retry in $retryTimer");
+    }
+    retryTimer = Timer(Duration(seconds: retryInterval), retryAction);
+    if (retryInterval < maxRetryInterval) {
+      retryInterval += retryIncrement;
     }
   }
 
   void sendCommand(String message) {
+    if (kDebugMode) {
+      print("sendCommand");
+    }
     if (socket != null) {
-      pid("send command: $message");
+      if (kDebugMode) {
+        print("send command: $message");
+      }
       socket!.write("noidle\n$message\n");
       connstate = ConnState.command;
     }
   }
 
   void processConnecting(lines) {
+    if (kDebugMode) {
+      print("processConnecting");
+    }
     if (lines.length == 1 && lines[0].startsWith("OK")) {
-      pid("connecting: getstatus");
+      print("processConnecting: getstatus");
       getStatus();
     } else {
-      pid("connecting: reconnecting");
-      reconnect();
+      if (kDebugMode) {
+        print("processConnecting: reconnecting");
+      }
+      reconnectSocket();
     }
   }
 
   void processCommand(List<String> lines) {
+    if (kDebugMode) {
+      print("processCommand");
+    }
     if (lines.length == 1 && lines.first == "OK") {
-      pid("command: goidle");
+      if (kDebugMode) {
+        print("processCommand: OK");
+      }
       goIdle();
     } else {
-      pid("command: reconnecting");
-      reconnect();
+      if (kDebugMode) {
+        print("processCommand: not OK");
+      }
+      reconnectSocket();
     }
   }
 
-  void pid(String msg) {
-    //print("${identityHashCode(this)}: $msg");
-  }
-
   void processMPDOutput(List<String> lines) {
+    if (kDebugMode) {
+      print("processMPDOutput");
+    }
     var error = false;
     var changed = false;
     var info = Info(connected: true); // info to be sent to the UI
@@ -212,19 +302,27 @@ class MPDClient {
     final okPattern = RegExp(r'^OK');
     final ackPattern = RegExp(r'^ACK');
     var sectionLineCount = 0;
-    pid("processing batch, state: $connstate");
+    if (kDebugMode) {
+      print("processMPDOutput: batch; state: $connstate");
+    }
     for (var line in lines) {
       if (error) {
-        pid("skipping because of error");
+        if (kDebugMode) {
+          print("processMPDOutput: skipping because of error");
+        }
         break; // ignore the rest of the lines
       }
-      pid("processing line: $line");
+      //print("processMPDOutput: processing line: $line");
       if (okPattern.hasMatch(line)) {
-        pid("OK detected");
+        if (kDebugMode) {
+          print("processMPDOutput: OK detected");
+        }
         // we found an "OK", signalling the end of a section of the response
         // if we processed more than one line on this pass, assume it was status output and process it
         if (sectionLineCount > 1) {
-          pid("processing metadata");
+          if (kDebugMode) {
+            print("processMPDOutput: processing metadata");
+          }
           // check various metadata scenarios, but only if state isn't stopped
           // for classical sometimes the artist tag contains the composer and
           // sometimes the performer; let's handle those situations as sensibly as
@@ -248,10 +346,15 @@ class MPDClient {
           info.addAll(InfoType.album, md["album"]);
           info.addAll(InfoType.station, md["name"]);
           if (info.hasData()) {
-            pid("state: sending ${info.info} sic ${info.subInfos.length}");
+            if (kDebugMode) {
+              print(
+                  "processMPDOutput: sending ${info.info} sic ${info.subInfos.length}");
+            }
             controller.add(info);
           } else {
-            pid("no data to send");
+            if (kDebugMode) {
+              print("processMPDOutput: no data to send");
+            }
           }
         }
         info = Info(
@@ -260,7 +363,9 @@ class MPDClient {
         md.clear();
         sectionLineCount = 0;
       } else if (ackPattern.hasMatch(line)) {
-        pid("error detected: $line");
+        if (kDebugMode) {
+          print("processMPDOutput: error detected: $line");
+        }
         error = true;
       } else {
         ++sectionLineCount;
@@ -270,7 +375,9 @@ class MPDClient {
           var value = match.group(2);
           switch (key) {
             case "changed":
-              pid("change detected");
+              if (kDebugMode) {
+                print("processMPDOutput: change detected");
+              }
               changed = true;
               break;
             case "repeat":
@@ -317,20 +424,35 @@ class MPDClient {
       }
     }
     if (error) {
-      reconnect();
+      if (kDebugMode) {
+        print("processMPDOutput: found error, trying reconnect");
+      }
+      reconnectSocket(); // onDone will reconnect
     } else if (changed) {
+      if (kDebugMode) {
+        print("processMPDOutput: changed, trying get status");
+      }
       getStatus();
     } else {
+      if (kDebugMode) {
+        print("processMPDOutput: go idle");
+      }
       goIdle();
     }
   }
 
   void goIdle() {
+    if (kDebugMode) {
+      print("goIdle");
+    }
     sendCommand("idle player");
     connstate = ConnState.readoutput;
   }
 
   void getStatus() {
+    if (kDebugMode) {
+      print("getStatus");
+    }
     sendCommand("command_list_begin\nstatus\ncurrentsong\ncommand_list_end");
     connstate = ConnState.readoutput;
   }
